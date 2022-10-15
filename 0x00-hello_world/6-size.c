098632e73164084f2229de36ebd3acae9b9bfac4
Bright Martins Ekong <brightblvd@gmail.com>
2022-10-15 16:39:06 -0700

    betty correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,11 @@
  */
 int main(void)
 {
-printf("Size of a char: %1d byte(s)\n", sizeof(char));
-printf("Size of an int: %1d byte(s)\n", sizeof(int));
-printf("size of a long int: %1d byte(s)\n", sizeof(long int));
-printf("Size of a long long int: %1d bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %1d byte(s)\n", sizeof(float));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu byte(s)\n", sizeof(int));
+printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
+return (0);
 }
