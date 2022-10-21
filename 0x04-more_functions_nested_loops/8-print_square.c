bd49c0c1aedc616a7debf88dc1b0aed0e34f7ac9
Bright Martins Ekong <brightblvd@gmail.com>
2022-10-21 16:58:36 -0700

    Write a function that prints a square, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: size of the sqaure
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('0');
+			}
+			_putchar('\n');
+		}
+	}
+}
