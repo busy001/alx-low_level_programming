d58505d2b67494dde2aa0f3c2aa8d887534fdf88
Bright Martins Ekong <brightblvd@gmail.com>
2022-11-09 01:31:50 -0800

    Create a script called create_static_lib.sh that creates a static library called liball.a from all the .c files that are in the current directory

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File: holberton.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file containing the prototypes of all functions
+ *       included in the static library libholberton.a.
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,5 +28,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
 
+#endif
